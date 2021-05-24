a2e20c9db6493a0edda17fa02d10ba8367f8337b
shreya-sridhar <shreyamohak@outlook.com>
2021-05-23 20:26:04 -0700

    data cleaning

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -3,4 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('reviews.csv')
+df1 = pd.read_csv('data/reviews.csv')
+
+def datatypeInfo():
+    df1.info()
+
+def viewDataSample():
+    print(df1.head())
+
+def centralTendency():
+    print("Ratings")
+    print(df1.rating.describe())
+    print("Dogs")
+    print(df1.dogs.describe())
+    print("Sitter")
+    print(df1.sitter.describe())
+
+datatypeInfo()
+viewDataSample()
+centralTendency()
+
+
