1becb2c706da73465b7f4eacc4a8ee7d3af07cc5
shreya-sridhar <shreyamohak@outlook.com>
2021-05-23 21:32:51 -0700

    cli interface done?

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,39 +1,36 @@
 require 'io/console'
 require 'tty-prompt'
 require 'pry'
-# require 'colorize'
-# require 'lolize'
+require 'colorize'
 require "tty-table"
+require 'tty-font'
 
 class CLI
-
-    attr_reader :prompt, :font
-    attr_accessor :customer, :cart, :pid, :current_user
-
     def main_menu
         system 'clear'
-        `say Welcome to The Cart`
         @font = TTY::Font.new
         @pastel = Pastel.new
-        opener
-        puts @pastel.cyan(@font.write("                                   Shopping        Cart !!"))
+        # opener
+        puts @pastel.cyan(@font.write("                                   ROVER CLI"))
 
-        opener
+        # opener
         prompt = TTY::Prompt.new
-        choices = ['🔹Login'.green ,'🔹Signup'.yellow, '🔹Exit'.red] # '🔹Update Name Info', '🔹Delete Account'
-        choice = prompt.select("\n please make a selection🔹\n", choices) # do not use multi_select it leaves an octogon symble 
-
-        if choice == '🔹Login'.green
-            attempts = 0
-            login(attempts)
-        elsif choice == '🔹Signup'.yellow
-            signup
-        elsif choice == '🔹Exit'.red
-            exit
-        end
+        choices = ['🔹DOWNLOAD SITTER RATINGS (IN CSV)'.green ,'🔹GET SITTER RATING BY NAME/EMAIL'.yellow, '🔹ADD SITTER RATING'.red]
+        choice = prompt.select("\n please make a selection🔹\n", choices) 
+        # do not use multi_select it leaves an octogon symble 
+        # if choice == '🔹Login'.green
+        #     attempts = 0
+        #     login(attempts)
+        # elsif choice == '🔹Signup'.yellow
+        #     signup
+        # elsif choice == '🔹Exit'.red
+        #     exit
+        # end
     end
 end
 
+cli = CLI.new()
+cli.main_menu()
 
 
 
