c9b6512aa0ca0fc2d8b283adbc78b72bced18545
shreya-sridhar <shreyamohak@outlook.com>
2021-05-24 23:25:08 -0700

    adding test cases

--- api/testing.rb
+++ api/testing.rb
@@ -1,13 +1,34 @@
 import unittest 
 
 class TestProfileScore(unittest.TestCase):
+    # when no distinct letters 
+
+    # all distinct letters
+
+    # combination of cases1&2 
+
+    # null name 
+
+    # name containing special characters like .
+
+    # name in upper and lower cases 
 
 
 
 class TestRatingsScore(unittest.TestCase):
+    # when no stays
+
+    # when large number of stays
+
+    # when all stays have 0 ratings 
 
 
 class TestSearchScore(unittest.TestCase):
+    # no stays 
+
+    # less than 10 stays
+
+    # 10 or more stays
 
 
 
