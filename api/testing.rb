d4eeed6c82b5f36de56ce1325acdb226887e7fba
shreya-sridhar <shreyamohak@outlook.com>
2021-05-25 00:06:55 -0700

    profile test cases

--- api/testing.rb
+++ api/testing.rb
@@ -2,22 +2,33 @@ import unittest
 
 class TestProfileScore(unittest.TestCase):
     # when no distinct letters 
+    def self.same_letters_test(self):
+        self.assertEqual(profile_score('j'),0)
 
     # all distinct letters
+    def self.distinct_letters_test(self):
+        self.assertEqual(profile_score('linda'),0)
 
     # combination of cases1&2 
+    def self.mixed_letters_test(self):
+        self.assertEqual(profile_score('casey sanders'),0)
 
     # null name 
+    def self.same_letters(self):
+        self.assertEqual(profile_score(''),0)
 
     # name containing special characters like .
+    def self.same_letters(self):
+        self.assertEqual(profile_score('samuel p. jones'),0)
 
     # name in upper and lower cases 
-
+    def self.same_letters(self):
+        self.assertEqual(profile_score('Harry Potter'),0)
 
 
 class TestRatingsScore(unittest.TestCase):
     # when no stays
-
+    
     # when large number of stays
 
     # when all stays have 0 ratings 
